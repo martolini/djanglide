430435ee4a98bff4d19b2289127deeec174f02d0
Carter Henderson <carterh062@gmail.com>
2014-01-26 23:20:29 -0500

    added jquery-ui

--- glide/app/booking/forms.py
+++ glide/app/booking/forms.py
@@ -3,10 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 from django.contrib.auth.models import User
 from glide.app.booking.models import Booked
-
+from glide.app.booking.fields import JqSplitDateTimeField
+from glide.app.booking.widgets import JqSplitDateTimeWidget
 class BookForm(ModelForm):
-	message = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Send a message to let them know about your visit!'}))
-	
+	message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Send a message to let them know about your visit!'}))
+
 	class Meta:
 		model = Booked
 		exclude = ('target_sender','target_recipient',)
--- glide/app/booking/models.py
+++ glide/app/booking/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Booked(models.Model):
-	DateTime = models.DateTimeField()
 	target_recipient = models.ForeignKey(User,verbose_name="The target recipient",related_name="book_recipients")
 	target_sender = models.ForeignKey(User,verbose_name="The target sender",related_name='book_targets',null=True)
 	message = models.TextField()
-
+	startTime = models.TimeField()
+	endTime = models.TimeField()
+	date = models.DateField()
 	def __unicode__(self):
 		return self.date
 
