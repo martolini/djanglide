c01b40e65a65380cc382737dc320e0f926f32e6f
Carter Henderson <carterh062@gmail.com>
2014-01-27 23:12:02 -0500

    minor backend changes to booking/views

--- glide/app/booking/views.py
+++ glide/app/booking/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
 from glide.app.booking.forms import BookForm
+
 def book_profile(request):
-	if request.method == 'POST': # If the form has been submitted...
+	 # If the form has been submitted...
+    form = BookForm(request.POST)
+    if form.is_valid():
+        date = form.cleaned_data['date']
+        time = form.cleaned_data['startTime']
+        message = form.cleaned_data['message']
+        seen = False
+        target_sender = request.user
 
-        form = BookForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): 
-        	date = form.cleaned_data['date']
-        	time = form.cleaned_data['startTime']
-        	message = form.cleaned_data['message']
-        	seen = False
-        	target_sender = request.user
-        	target_recipient = 
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = BookForm() # An unbound form
-
-    return render(request, 'contact.html', {
+    return render(request, 'base.html', {
         'form': form,
     })
